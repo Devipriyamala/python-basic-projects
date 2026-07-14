51ca24f4f15a6002686a35ca124718ac81405bfe
Devipriyamala <devipriya240106@gmail.com>
2026-07-15 01:08:16 +0530

    Add student ID card details program

---
+++ 01_student_id/student_id.py
@@ -0,0 +1,14 @@
+#Project 1: Student ID card Details
+
+print("----STUDENT ID CARD----")
+name=input("Enter your Name:")
+roll_no=int(input("Enter your Roll Number:"))
+department=input("Enter your Department:")
+college=input("Enter your College Name:")
+
+print("----STUDENT ID CARD----")
+
+print(f"Name: {name}")
+print(f"Roll Number: {roll_no}")
+print(f"Department: {department}")
+print(f"College: {college}")
