bd07b4a2c663d3bd3a5a3a530b815ac99da9d60a
Devipriyamala <devipriya240106@gmail.com>
2026-09-21 20:37:54 +0530

    Add simple age calculator
    A simple program that calculates a person's age based on their birth year and current year.

---
+++ 13_age_calculator/age_calculator.py
@@ -0,0 +1,11 @@
+#Simple Age Calculator
+
+name = input("Enter your name: ")
+birth_year = int(input("Enter your birth year: "))
+current_year = int(input("Enter current year: "))
+age = current_year - birth_year
+print("-----AGE CALCULATOR-----")
+print("Name: ",name)
+print("Birth Year: ",birth_year)
+print("Current Year: ",current_year)
+print("Age: ",age)
