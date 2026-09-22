47523188680ae74dfa457e8a4f99b474f8d3b295
Devipriyamala <devipriya240106@gmail.com>
2026-09-22 23:16:41 +0530

    Add simple temperature converter from Celsius to Fahrenheit
    Converts Temperature from Celsius to Fahrenheit.

---
+++ 14_temperature_converter/temperature_converter.py
@@ -0,0 +1,7 @@
+#Simple Temperature Converter that converts celsius to fahrenheit
+
+celsius = int(input("Enter Temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print("-----TEMPERATURE CONVERTER-----")
+print("Celsius: ",celsius)
+print("Fahrenheit: ",fahrenheit)
