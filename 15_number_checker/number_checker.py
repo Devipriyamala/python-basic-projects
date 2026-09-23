0362401bda81472c1d2bcf94f48575f74dc689e8
Devipriyamala <devipriya240106@gmail.com>
2026-09-23 22:40:41 +0530

    Add simple number checker script
    Checks whether a given number is positive, negative, or zero.

---
+++ 15_number_checker/number_checker.py
@@ -0,0 +1,12 @@
+#Simple Number Checker
+
+num = int(input("Enter a number: "))
+print("-----NUMBER CHECKER-----")
+print("Number: ",num)
+if num > 0:
+  print("Result: Positive Number")
+elif num < 0:
+  print("Result: Negative Number")
+else:
+  print("Result: Zero")
+  
